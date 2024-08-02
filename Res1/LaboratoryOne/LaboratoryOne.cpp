f9d57f9972797bcc4ae684c1696fb765d60012a5
DanielRY <97177840+RYDanielRY@users.noreply.github.com>
2024-08-02 11:37:08 +0200

    Two

---
+++ Res1/LaboratoryOne/LaboratoryOne.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Rus");
+    double a; double b;
+    cout << "Введите катеты: A, B" << endl;
+    cin >> a;
+    cin >> b;
+    double c = sqrt(pow(a, 2.0) + pow(b, 2.0));
+    cout << "Гипотенуза равна: " << c << endl;
+    return 0; 
+}
